4bdb9bd97da89b904f82c16da98be00d2815b0f2
SamuelAmbrosio <samuelambrosio99@gmail.com>
2020-10-01 12:03:13 -0600

    fix zoom

--- resources/views/web/default/view/layout/header.blade.php
+++ resources/views/web/default/view/layout/header.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="{!! get_option('site_description','') !!}">
     <link rel="stylesheet" href="/assets/default/vendor/bootstrap/css/bootstrap.min.css" />
