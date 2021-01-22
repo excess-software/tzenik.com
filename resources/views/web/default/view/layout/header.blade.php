e1746869565e5f3e6129361a4c59f3d65f6228a0
SamuelAmbrosio <samuelambrosio99@gmail.com>
2021-01-21 23:12:53 -0600

    added socket.io

--- resources/views/web/default/view/layout/header.blade.php
+++ resources/views/web/default/view/layout/header.blade.php
@@ -72,6 +72,9 @@
     </style>
     @endif
     <script type="application/javascript" src="/assets/default/vendor/jquery/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"
+        integrity="sha512-v8ng/uGxkge3d1IJuEo6dJP8JViyvms0cly9pnbfRxT6/31c3dRWxIiwGnMSWwZjHKOuY3EVmijs7k1jz/9bLA=="
+        crossorigin="anonymous"></script>
     <title>@yield('title'){!! $title ?? '' !!}</title>
     <script>
         function changeFont(font) {
