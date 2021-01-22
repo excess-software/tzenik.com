f24a4c0db1bef4e39d9592c7b25101cceeb678a3
SamuelAmbrosio <samuelambrosio99@gmail.com>
2021-01-21 23:34:37 -0600

    socket.io

--- resources/views/web/default/view/layout/header.blade.php
+++ resources/views/web/default/view/layout/header.blade.php
@@ -124,7 +124,8 @@
     </script>
     @if(isset($user))
     <script>
-        var socket = io.connect('https://157.245.94.48:8890', {secure: true});
+        var origin = window.location.origin;
+        var socket = io.connect(origin);
         $(document).ready(function () {
             var host = window.location.origin;
             var message_id = '';
