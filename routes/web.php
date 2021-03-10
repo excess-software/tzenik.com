449b3e1bea533f002d026b69c5d238ed2d850c50
SamuelAmbrosio <samuelambrosio99@gmail.com>
2021-03-10 13:07:31 -0600

    password reset

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('testForm', 'WebController@testForm');
 
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::group(['namespace' => 'Auth'], function () {
     // Web Auth Routes
