20035027457fb30561f5f69ed1ee826b100f38de
SamuelAmbrosio <samuelambrosio99@gmail.com>
2021-03-10 13:11:06 -0600

    reset password

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -19,4 +19,9 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+    public function showLinkRequestForm()
+    {
+        return view('Web.default.auth.login');
+    }
 }
