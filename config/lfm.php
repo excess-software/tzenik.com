bc7d9a479a57c236b2546811411fd4f077153fe2
SamuelAmbrosio <samuelambrosio99@gmail.com>
2021-03-11 13:46:15 -0600

    lfm limit

--- config/lfm.php
+++ config/lfm.php
@@ -52,7 +52,7 @@ return [
         'file' => [
             'folder_name' => '/',
             'startup_view' => 'list',
-            'max_size' => 50000, // size in KB
+            'max_size' => 2000000, // size in KB
             'valid_mime' => [
                 '.pdf',
                 '.doc',
